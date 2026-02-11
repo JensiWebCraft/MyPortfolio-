42ea610a699f6299b9870acc3d03358c9955234e
jensiwebcraft <jensikachhadiya4@gmail.com>
2026-02-11 14:32:50 +0530

    add

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import './Hero.css';
-import profileImg from '../assets/image.png'; // Replace with your circular photo
-import { Link } from 'react-router-dom';
+import React from "react";
+import "./Hero.css";
+import profileImg from "../assets/myimage.png"; // Replace with your circular photo
+import { Link } from "react-router-dom";
 
 const Hero = () => {
   return (
@@ -9,20 +9,28 @@ const Hero = () => {
       <div className="hero-content">
         <img src={profileImg} alt="Profile" className="hero-img" />
         <h1 className="hero-title">
-          I'm <span className="purple">Jensi</span> <span className="orange">Kachhadiya</span>,<br />
-          Web & App Developer In  <span className="white">India</span>.
+          I'm <span className="purple">Jensi</span>{" "}
+          <span className="orange">Kachhadiya</span>,<br />
+          Web & App Developer In <span className="white">India</span>.
         </h1>
         <p className="hero-subtext">
-          I'm skilled in MERN, Firebase, and Flutter, currently studying Computer Engineering.
+          I'm skilled in MERN, Firebase, and Flutter, currently studying
+          Computer Engineering.
         </p>
 
         <div className="hero-buttons">
-          <Link to="/contact" className="btn-gradient">Connect with Me</Link>
-          <a href="/C:/Users/jensi/Downloads/Jensi Kachhdiya Resume.pdf" download className="btn-outline">My Resume</a>
-
+          <Link to="/contact" className="btn-gradient">
+            Connect with Me
+          </Link>
+          <a
+            href="/C:/Users/jensi/Downloads/Jensi Kachhdiya Resume.pdf"
+            download
+            className="btn-outline"
+          >
+            My Resume
+          </a>
         </div>
       </div>
-    
     </section>
   );
 };
